d8532e3c0b729e5860254a7da9123746e45f2014
Eamonn Maguire <eamonnmag@gmail.com>
2011-03-02 18:15:32 +0000

    first commit of template project code

---
+++ src/main/java/org/isatools/magetoisatab/Converter.java
@@ -0,0 +1,15 @@
+/**
+ MAGETabtoISATab is a component of the ISA software suite (http://www.isa-tools.org)
+
+ Sponsors:
+ The ISA Team and the ISA software suite have been funded by the EU Carcinogenomics project (http://www.carcinogenomics.eu),
+ the UK BBSRC (http://www.bbsrc.ac.uk), the UK NERC-NEBC (http://nebc.nerc.ac.uk) and in part by the EU NuGO consortium (http://www.nugo.org/everyone).
+ */
+
+package org.isatools.magetoisatab;
+
+/**
+ * Main entry class to convert MAGETab to ISAtab given a directory containing MAGE-Tab files
+ */
+public class Converter {
+}
---
+++ src/main/java/org/isatools/magetoisatab/io/MAGETabLoader.java
@@ -0,0 +1,57 @@
+package org.isatools.magetoisatab.io;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by the ISA team
+ *
+ * @author Eamonn Maguire (eamonnmag@gmail.com)
+ *         <p/>
+ *         Date: 02/03/2011
+ *         Time: 18:02
+ */
+public class MAGETabLoader {
+
+    public static final Character TAB_DELIM = '\t';
+
+    private void loadMAGETab(String MAGETabDirectory) throws IOException {
+
+        File mageTaDir = new File(MAGETabDirectory);
+
+        if (mageTaDir.exists()) {
+            if (mageTaDir.isDirectory()) {
+
+                File[] mageTabFiles = mageTaDir.listFiles();
+
+                for (File file : mageTabFiles) {
+                    CSVReader fileReader = new CSVReader(new FileReader(file), TAB_DELIM);
+
+                    // you can read each line separately!
+                    String[] nextLine;
+
+                    while ((nextLine = fileReader.readNext()) != null) {
+
+                        // do whatever you want with the line
+
+                    }
+
+
+                    // or you can get the whole file as a list of String arrays. Each list item is a row.
+                    List<String[]> myEntries = fileReader.readAll();
+                }
+
+
+
+            } else {
+                // throw an Exception declaring that the given path is not a directory
+            }
+        }
+
+    }
+}
