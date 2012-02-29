6eee77468c77214f1bb87823049fd4108915ebe5
Philippe Rocca-Serra <proccaserra@gmail.com>
2012-02-29 17:57:12 +0000

    refactoring of MAGE2ISAconverter to cope with multiple sdrf, addressing prb in MAGE-TAB files

---
+++ src/main/java/org/isatools/magetoisatab/io/model/Study.java
@@ -0,0 +1,11 @@
+package org.isatools.magetoisatab.io.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: prs
+ * Date: 23/02/2012
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Study {
+}
---
+++ src/main/java/org/isatools/magetoisatab/utils/PrintUtils.java
@@ -0,0 +1,11 @@
+package org.isatools.magetoisatab.utils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: prs
+ * Date: 24/02/2012
+ * Time: 10:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrintUtils {
+}
